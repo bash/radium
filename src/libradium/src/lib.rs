55e3b4f7956e30237dcb178ff873c6d92541c8c0
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-04-16 15:03:07 +0200

    Remove unused code, dependencies

--- src/libradium/src/lib.rs
+++ src/libradium/src/lib.rs
@@ -1,13 +1,7 @@
 #![crate_name = "libradium"]
 
 extern crate rand;
-extern crate threadpool;
-extern crate num_cpus;
-extern crate byteorder;
 pub extern crate time;
-
-#[macro_use]
-mod macros;
 
 pub mod entry;
 pub mod frontend;
--- src/libradium/src/macros.rs
+++
@@ -1,9 +0,0 @@
-#[macro_export]
-macro_rules! maybe {
-    ($value:expr) => {
-        match $value {
-            None => { return None },
-            Some(value) => { value }
-        }
-    }
-}
