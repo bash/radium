d9749eb3b6c5e5b36d55dc0cc76980b226c6c427
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-05-28 12:49:00 +0200

    Use getopts for host and port

--- src/radiumd/src/main.rs
+++ src/radiumd/src/main.rs
@@ -1,4 +1,5 @@
 extern crate libradium;
+extern crate getopts;
 #[macro_use]
 extern crate log;
 extern crate mio;
@@ -6,6 +7,8 @@ extern crate mio_channel;
 extern crate radium_protocol;
 extern crate slab;
 
+#[macro_use]
+mod macros;
 mod actions;
 mod connection;
 mod server;
@@ -14,20 +17,17 @@ mod pool;
 mod entry;
 mod worker;
 
+use getopts::Options;
 use libradium::{Frontend, Listener, Timestamp};
 use logger::Logger;
 use mio_channel::{channel, Sender};
 use mio::tcp::TcpListener;
 use pool::Pool;
+use std::env;
+use std::net::SocketAddr;
 
 use self::server::Server;
 use self::entry::{Entry, EntryData};
-
-macro_rules! sock_addr {
-    ($a:expr, $b:expr, $c:expr, $d:expr, $port:expr) => {
-        ::std::net::SocketAddr::new(::std::net::IpAddr::V4(::std::net::Ipv4Addr::new($a, $b, $c, $d)), $port);
-    }
-}
 
 struct EntryListener {
     sender: Sender<Entry>
@@ -40,7 +40,31 @@ impl Listener<EntryData> for EntryListener {
 }
 
 fn main() {
-    let addr = sock_addr!(127, 0, 0, 1, 3126);
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+
+    opts.optopt("H", "host", "sets the host to listen on", "HOST");
+    opts.optopt("P", "port", "set port to listen on", "PORT");
+    opts.optflag("h", "help", "print this help menu");
+
+    let matches = opts.parse(&args[1..]).unwrap();
+
+    let host = match matches.opt_str("H") {
+        Some(val) => { val }
+        None => "127.0.0.1".to_string()
+    };
+
+    let port = match matches.opt_str("P") {
+        Some(val) => { val }
+        None => "3126".to_string()
+    };
+
+    let addr = format!("{}:{}", host, port).parse::<SocketAddr>().unwrap();
+
+    println!("Listening on {:?}", addr);
+
     let tcp = TcpListener::bind(&addr).unwrap();
 
     // TODO: cli flags --host, --port, --verbose
