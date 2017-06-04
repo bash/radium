9bc2f0df6428e333f04aa1315feb8b4be3c19316
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-06-04 09:52:40 +0200

    Update build script

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,13 +1,50 @@
 #!/bin/sh
 
-SRC_DIR="${TRAVIS_BUILD_DIR}/src"
+set -e
+
+fold_start () {
+  if [ ! -z "${TRAVIS}" ]; then
+    echo -en "travis_fold:start:${1}\\r"
+  else
+    tput setaf 4
+    echo "=> ${1}"
+    tput sgr0
+  fi
+}
+
+fold_end () {
+  if [ ! -z "${TRAVIS}" ]; then
+    echo -en "travis_fold:end:${1}\\r"
+  fi
+}
+
+SRC_DIR="`pwd`/src"
 
 for DIR in $(ls "${SRC_DIR}")
 do
     cd "${SRC_DIR}/${DIR}"
 
-    cargo fmt -- --write-mode=diff
+    FEATURES=""
+
+    if [ "${DIR}" = "libradium" ]; then
+        FEATURES="--all-features"
+    fi
+
+    fold_start "Running rustfmt for ${DIR}"
+    # cargo fmt -- --write-mode=diff || true
+    fold_end "Running rustfmt for ${DIR}"
+
+    fold_start "Building ${DIR}"
     cargo build
-    cargo build --example main
-    cargo test
+    fold_end "Building ${DIR}"
+
+    if [ "${DIR}" = "libradium" ]; then
+        fold_start "Building ${DIR} example"
+        cargo build ${FEATURES} --example main
+        fold_end "Building ${DIR} example"
+    fi
+
+    fold_start "Testing ${DIR}"
+    cargo test ${FEATURES}
+    fold_end "Testing ${DIR}"
 done
