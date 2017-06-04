6680172b0eb907931cf54179ffd997534327a8c1
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-06-04 09:00:00 +0200

    Prepare rust client

--- src/radium/src/main.rs
+++ src/radium/src/main.rs
@@ -1,3 +1,34 @@
+extern crate radium_protocol;
+
+use std::net::TcpStream;
+use radium_protocol::{Message, Reader, WriteValueExt, SyncReaderController, WatchMode};
+use radium_protocol::messages::{AddEntry, SetWatchMode};
+use std::env;
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mode = std::env::var("RADIUM_MODE").unwrap_or(String::from(""));
+
+    let mut stream = TcpStream::connect("127.0.0.1:3126").unwrap();
+    let mut reader = SyncReaderController::new(Message::reader());
+
+    stream.write_value(&Message::Ping).unwrap();
+    let message = reader.resume(&mut stream).unwrap();
+    println!("{:?}", message);
+    io::stdout().flush().unwrap();
+
+    stream.write_value(&Message::AddEntry(AddEntry::new(0, 123, vec![1, 2, 3])));
+    let message = reader.resume(&mut stream).unwrap();
+    println!("{:?}", message);
+
+    if mode == "w" {
+        stream.write_value(&Message::SetWatchMode(SetWatchMode::new(WatchMode::All))).unwrap();
+
+        loop {
+            let message = reader.resume(&mut stream).unwrap();
+
+            println!("{:?}", message);
+        }
+    }
 }
