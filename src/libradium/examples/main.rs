d89031a932956b033bebfba77956c16c4a1b4185
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-09-17 10:39:05 +0200

    Simplify example

--- src/libradium/examples/main.rs
+++ src/libradium/examples/main.rs
@@ -5,108 +5,49 @@ use std::io::prelude::*;
 use std::io;
 use std::sync::mpsc::{Sender, Receiver};
 
-use libradium::{Entry, Timestamp, Listener, Frontend};
+use libradium::{Entry, Timestamp, Frontend};
 
-#[derive(Debug)]
-struct User {
-    age: i64,
+type Data = ();
+type Entries<T> = Vec<Entry<T>>;
+
+struct Listener {
+    tx: Sender<Entries<Data>>,
 }
 
-impl User {
-    pub fn new(age: i64) -> Self {
-        User { age }
-    }
-
-    pub fn age(&self) -> i64 {
-        self.age
-    }
-}
-
-struct TestListener {
-    tx: Sender<Output>,
-}
-
-enum Output {
-    Expired(Vec<Entry<User>>),
-    #[cfg(feature = "with-ticks")]
-    Tick,
-}
-
-impl Listener<User> for TestListener {
-    fn on_expired(&self, entries: Vec<Entry<User>>) {
-        self.tx.send(Output::Expired(entries)).unwrap();
-    }
-
-    #[cfg(feature = "with-ticks")]
-    fn on_tick(&self) {
-        self.tx.send(Output::Tick).unwrap();
+impl libradium::Listener<Data> for Listener {
+    fn on_expired(&self, entries: Entries<Data>) {
+        self.tx.send(entries).unwrap();
     }
 }
 
 fn main() {
-    let (tx_listener, rx_listener): (Sender<Output>, Receiver<Output>) = mpsc::channel();
-    let listener = Box::new(TestListener { tx: tx_listener });
+    let (tx_listener, rx_listener): (Sender<Entries<Data>>, Receiver<Entries<Data>>) =
+        mpsc::channel();
+    let listener = Box::new(Listener { tx: tx_listener });
     let (frontend, _) = Frontend::build(listener);
 
     let now = Timestamp::now();
 
-   /* frontend
-        .add_entry(Entry::gen(now, User::new(10)))
-        .unwrap();
-
-    frontend
-        .add_entry(Entry::gen(now + 2, User::new(20)))
-        .unwrap();
-
-    frontend
-        .add_entry(Entry::gen(now + 4, User::new(30)))
-        .unwrap();
-
-    frontend
-        .add_entry(Entry::gen(now + 6, User::new(40)))
-        .unwrap();
-
-    frontend
-        .add_entry(Entry::gen(now + 6, User::new(50)))
-        .unwrap();
-
-    frontend
-        .add_entry(Entry::gen(now + 6, User::new(60)))
-        .unwrap();
-
-    frontend
-        .add_entry(Entry::gen(now + 6, User::new(70)))
-        .unwrap();
-
-    frontend
-        .add_entry(Entry::gen(now + 10, User::new(80)))
-        .unwrap();*/
-
     for i in 0..100 {
-        frontend
-            .add_entry(Entry::gen(now + (i * 10), User::new(i)))
-            .unwrap();
+        frontend.add_entry(Entry::gen(now + (i * 10), ())).unwrap();
 
         frontend
-            .add_entry(Entry::gen(now + (i * 10) + 1, User::new(i + 100)))
+            .add_entry(Entry::gen(now + (i * 10) + 1, ()))
             .unwrap();
     }
 
     loop {
-        match rx_listener.recv().unwrap() {
-            Output::Expired(entries) => {
-                for entry in entries {
-                    print!("({:?}, {})", entry.id().timestamp().sec, entry.data().age());
-                }
+        let entries = rx_listener.recv().unwrap();
+        let len = entries.len();
 
-                // frontend.add_entry(Entry::gen(Timestamp::now() + 20, User::new(Timestamp::now().sec)));
+        for entry in entries {
+            print!("( {:?} )", entry.id().timestamp().sec);
+        }
 
-                io::stdout().flush().unwrap();
-            }
-            #[cfg(feature = "with-ticks")]
-            Output::Tick => {
-                print!(".");
-            }
-        };
+        if len > 0 {
+            println!();
+        }
+
+        let _ = io::stdout().flush();
     }
 }
